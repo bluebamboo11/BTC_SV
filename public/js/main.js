0042a44e55e7cd2091b9a3c9d2e3a8aaf8032e31
bluebamboo11 <tongthanhtung315@gmail.com>
2018-03-07 23:44:56 +0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,65 @@
 angular.module('MyApp', ['ngMaterial'])
     .controller('AppCtrl', function ($scope, $http, $mdDialog, $mdToast) {
-        $scope.lastPrice=0;
-        $scope.bidPrice =0;
-        GetMarket();
+        $scope.lastPrice = 0;
+        $scope.bidPrice = 0;
+
         function GetMarket() {
             $http({
                 method: 'GET',
                 url: 'getInfo'
             }).then(function successCallback(response) {
-                $scope.lastPrice=response.data.last;
-                $scope.bidPrice=response.data.low
+                $scope.lastPrice = response.data.last;
+                $scope.bidPrice = response.data.low
+            }, function errorCallback(response) {
+
+            });
+        }
+
+        $scope.addTradeBuy = function () {
+            $http({
+                method: 'POST',
+                url: 'addTradeBuy',
+                data:
+                    {
+                        apiKey: $scope.apiKey,
+                        apiSecret: $scope.apiSecret,
+                        amount: $scope.amount,
+                        percent: $scope.percent,
+                        code: $scope.code
+                    },
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                }
+            }).then(function successCallback(response) {
+                console.log(response.data);
+                if (response.data.error) {
+                    $scope.lastPrice = response.data.last;
+                    $scope.bidPrice = response.data.low
+                }{
+                    alert('Lỗi : '+response.data);
+                }
+            }, function errorCallback(response) {
+
+            });
+        }
+        $scope.addTradeSell = function () {
+            $http({
+                method: 'POST',
+                url: 'addTradeSell',
+                data:
+                    {
+                        apiKey: $scope.apiKey,
+                        apiSecret: $scope.apiSecret,
+                        amount: $scope.amount,
+                        percent: $scope.percent,
+                        code: $scope.code
+                    },
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                }
+            }).then(function successCallback(response) {
+                console.log(response.data);
+
             }, function errorCallback(response) {
 
             });
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,67 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 Yobit = require('yobit');
 
 // Test public data APIs
 var publicClient = new Yobit();
-var privateClient = new Yobit('', '')
+
 
 router.get('/getInfo', function (req, res, next) {
-    publicClient.getTicker(function(err,data){
+    publicClient.getTicker(function (err, data) {
         console.log(data);
         res.send(data['eth_btc']);
-        return true}, 'eth_btc')
+        return true
+    }, 'eth_btc')
 });
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.sendFile('index.html');
 });
+router.post('/addTradeBuy', function (req, res, next) {
+    if (!req.body) return res.sendStatus(400);
+    var data = req.body;
+    buyPrice(data, res);
+});
+router.post('/addTradeSell', function (req, res, next) {
+    if (!req.body) return res.sendStatus(400);
+    var trade = req.body;
+    publicClient.getOrderBook(function (err, dataOrder) {
+        var privateClient = new Yobit(trade.apiKey, trade.apiSecret);
+        privateClient.getInfo(function (err, dataInfo) {
+            console.log(dataInfo.return.funds.btc+'--'+dataOrder[trade.code].bids[0][0]);
+            // privateClient.addTrade(function (err, dataTrade) {
+            //
+            // }, trade.code, 'sell', dataInfo.return.funds.eth, price);
+            res.send(dataInfo);
+        });
+    }, trade.code, 1)
+});
+
+function buyPrice(trade, res) {
+    console.log(trade);
+    publicClient.getTicker(function (err, data) {
+        if (err) {
+            res.send(err);
+        } else {
+            if (data[trade.code]) {
+                var price = data[trade.code].low * (1 + trade.percent / 100);
+                var amount = trade.amount / price;
+                var privateClient = new Yobit(trade.apiKey, trade.apiSecret);
+                privateClient.addTrade(function (err, dataTrade) {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        dataTrade.last = data[trade.code].last;
+                        dataTrade.low = data[trade.code].low;
+                        res.send(dataTrade);
+                    }
+                }, trade.code, 'buy', amount, price);
+            } else {
+                res.send(data);
+            }
+        }
+    }, trade.code)
+}
 
 module.exports = router;
